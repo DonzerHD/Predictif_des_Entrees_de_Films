23df03196899fee0b701095c1691452eb5541e08
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2023-08-09 14:38:21 +0200

    documentation de l'analyse et des fichiers scrapy films

--- scrapy_films/scrapy_films/pipelines.py
+++ scrapy_films/scrapy_films/pipelines.py
@@ -1,22 +1,56 @@
-import pyodbc
+# Importer les bibliothèques nécessaires
 from dotenv import load_dotenv
 import os
+import pyodbc
 
-load_dotenv()  # take environment variables from .env.
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()  # Prend les variables d'environnement à partir du fichier .env.
 
 class SqlServerPipeline(object):
+    """
+    Un pipeline pour le web scraping de films vers une base de données SQL Server.
+    """
 
     def open_spider(self, spider):
-        self.connection = pyodbc.connect(f'DRIVER={"ODBC Driver 18 for SQL Server"};SERVER={os.getenv("DB_SERVER")};DATABASE={os.getenv("DB_DATABASE")};UID={os.getenv("DB_UID")};PWD={os.getenv("DB_PWD")}')
+        """
+        Ouvre la connexion à la base de données SQL Server.
+
+        Parameters:
+        spider (scrapy.Spider): L'araignée de scraping en cours d'exécution.
+        """
+        self.connection = pyodbc.connect(
+            f'DRIVER={"ODBC Driver 18 for SQL Server"};SERVER={os.getenv("DB_SERVER")};DATABASE={os.getenv("DB_DATABASE")};UID={os.getenv("DB_UID")};PWD={os.getenv("DB_PWD")}'
+        )
         self.cursor = self.connection.cursor()
 
     def close_spider(self, spider):
+        """
+        Ferme la connexion à la base de données SQL Server.
+
+        Parameters:
+        spider (scrapy.Spider): L'araignée de scraping en cours d'exécution.
+        """
         self.connection.close()
 
     def process_item(self, item, spider):
-        self.cursor.execute("""
+        """
+        Insère les données extraites dans la table "film_scrapy" de la base de données.
+
+        Parameters:
+        item (dict): Les données extraites de l'araignée.
+        spider (scrapy.Spider): L'araignée de scraping en cours d'exécution.
+
+        Returns:
+        dict: Les données extraites inchangées.
+        """
+        self.cursor.execute(
+            """
             INSERT INTO dbo.film_scrapy (title, entries, date)
             VALUES (?, ?, ?)
-        """, (item['title'], item['entries'], item['date']))
+            """,
+            (item['title'], item['entries'], item['date'])
+        )
         self.connection.commit()
         return item
+
+
